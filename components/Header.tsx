03cd64ebdecbddf79274907112957def2c83b53d
MJanowskiDev <mat.krz.janowski@gmail.com>
2022-10-09 19:05:04 +0200

    Added ActiveLink component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,15 @@
-import Link from "next/link";
+import { ActiveLink } from "./ActiveLink";
 
 export const Header = () => {
   return (
     <header className="mx-auto w-full max-w-2xl ">
       <nav className="bg-gray-800 px-4 py-2 text-white flex gap-4 ">
-        <Link href={"/"}>
+        <ActiveLink href={"/"}>
           <a>Home</a>
-        </Link>
-        <Link href={"/about"}>
+        </ActiveLink>
+        <ActiveLink href={"/about"}>
           <a>About</a>
-        </Link>
+        </ActiveLink>
       </nav>
     </header>
   );
