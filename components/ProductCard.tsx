e2f68bbfdd0989ee7705d661f05e9d2c68ebb389
MJanowskiDev <mat.krz.janowski@gmail.com>
2022-11-02 15:58:59 +0100

    Renamed utills to utils

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { ProductsAPIResponse } from "../utills";
+import { ProductsAPIResponse } from "../utils";
 
 interface ProductCardProps {
   data: Pick<ProductsAPIResponse, "title" | "description" | "image" | "id">;
