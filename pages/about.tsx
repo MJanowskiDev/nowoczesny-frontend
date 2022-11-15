165d515fc0159db5e85da061820a6f6308e6a19a
MJanowskiDev <mat.krz.janowski@gmail.com>
2022-11-15 20:21:39 +0100

    Added about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,9 +1,42 @@
 import { NextSeo } from "next-seo";
+import Image from "next/image";
+import aboutImage from "../assets/images/about.svg";
 const About = () => {
   return (
     <div>
       <NextSeo title="About" description="About MJanowskiDev E-Commerce App" />
-      <main>About page</main>
+      <main className="w-full h-[calc(100vh_-_168px)] flex justify-center">
+        <article className=" h-full w-full prose dark:prose-invert flex flex-col justify-center">
+          <h1>
+            Homework project along{" "}
+            <span className="text-teal-600 dark:text-teal-300 ">
+              NOWOCZESNY FRONTEND
+            </span>{" "}
+            course
+          </h1>
+          <div className="max-w-4xl">
+            <Image
+              src={aboutImage}
+              alt={"MJanowskiDev ecommerce shop - about"}
+              layout="responsive"
+              width={16}
+              height={9}
+              objectFit="contain"
+            />
+          </div>
+          <h2 className="text-right">
+            The course covers:{" "}
+            <span className="text-teal-600 dark:text-teal-300 ">
+              Next.js, React, GraphQL and TypeScript
+            </span>
+            !
+          </h2>
+
+          <a href="https://hyperfunctor.com/nextjs-react-graphql-typescript">
+            Detailed course program
+          </a>
+        </article>
+      </main>
     </div>
   );
 };
