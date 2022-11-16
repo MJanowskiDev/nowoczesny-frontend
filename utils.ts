c5870d4c40f4a23a05ad474239bc7056e052d9b4
MJanowskiDev <mat.krz.janowski@gmail.com>
2022-11-16 19:14:51 +0100

    Added code-along session code with reacth-hook-form and yup

--- utils.ts
+++ utils.ts
@@ -22,3 +22,26 @@ export const PRODUCT_TAKE_AMOUNT = 10;
 export type MDXResult = MDXRemoteSerializeResult<Record<string, unknown>>;
 
 export const externalLinkRegex = "@https|http|ftp|mailto|file";
+
+/**
+ *
+ * @param value : string - card year month
+ * @returns string if validation error, bool true if validation ok
+ */
+export const validateCardYearMonth = (value: string) => {
+  if (value.length !== 5) {
+    return "Wrong data format, enter MM/YY";
+  }
+
+  const [month] = value.split("/");
+
+  if (Number(month) > 12) {
+    return "Wrong month";
+  }
+
+  if (Number(month) < 1) {
+    return "Wrong month";
+  }
+
+  return true;
+};
