caa8c6e14ce91df8382e619590a9f69e1da9c05e
MJanowskiDev <mat.krz.janowski@gmail.com>
2022-12-06 15:04:08 +0100

    Added authenticated apollo client using secret hygraph token

--- graphql/apolloClient.ts
+++ graphql/apolloClient.ts
@@ -4,3 +4,11 @@ export const apolloClient = new ApolloClient({
   uri: process.env.NEXT_PUBLIC_CMS_URL,
   cache: new InMemoryCache(),
 });
+
+export const authorizedApolloClient = new ApolloClient({
+  uri: "https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/cla8iu8x60ux401t5hoqfe3k7/master",
+  cache: new InMemoryCache(),
+  headers: {
+    Authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}`,
+  },
+});
